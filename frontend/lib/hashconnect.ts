ed65dc5ca9c89f782fe30d7af00e15bb6f02b797
FlyGuy352 <tom.lin@aodigy.com>
2025-11-15 11:36:21 +0800

    Load HashConnect only in browser

--- frontend/lib/hashconnect.ts
+++ frontend/lib/hashconnect.ts
@@ -1,17 +1,20 @@
-import { HashConnect } from "hashconnect";
 import { LedgerId, AccountId, TopicMessageSubmitTransaction } from "@hashgraph/sdk";
 
-const hc = new HashConnect(
-    LedgerId.fromString("testnet"),
-    process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!, // projectId
-    {
-        name: "ZK Voting",
-        description: "ZK Voting - Hedera Hashgraph DApp",
-        icons: [`${window.location.origin}/favicon.ico`],
-        url: "http://localhost:3000",
-    },
-    true
-);
+let hc;
+if (typeof window !== "undefined") {
+    const { HashConnect } = await import("hashconnect");
+    hc = new HashConnect(
+        LedgerId.fromString("testnet"),
+        process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!, // projectId
+        {
+            name: "ZK Voting",
+            description: "ZK Voting - Hedera Hashgraph DApp",
+            icons: [`${window.location.origin}/favicon.ico`],
+            url: "http://localhost:3000",
+        },
+        true
+    );
+}
 
 export const getHashConnectInstance = (): HashConnect => {
     if (!hc) {
